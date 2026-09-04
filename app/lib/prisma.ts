d88a7e3e00bf040445d7f2ed39b59bdd5babdcf7
Joseph Silcock <joseph.silcock@softwire.com>
2026-09-04 09:42:36 +0100

    Update packages

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter: new PrismaPg(process.env.DATABASE_URL!),
     log: ["error"],
   });
